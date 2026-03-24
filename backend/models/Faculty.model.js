316fee8a8d8967646398421b515eb483b6550a2d
Farhan Tanvir <56257502+Immobile07@users.noreply.github.com>
2026-03-24 19:59:20 +0600

    Delete backend directory

--- backend/models/Faculty.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const facultySchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  department: { type: String, required: true },
-  coursesTaught: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Course' }]
-}, { timestamps: true });
-
-module.exports = mongoose.model('Faculty', facultySchema);
